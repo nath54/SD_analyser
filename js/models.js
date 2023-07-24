b7992f290f168fa1b25a6aa39b351a4a3dbca617
nathan cerisara <cerisara.nathan@gmail.com>
2023-07-24 11:07:05 +0200

    Website update

--- js/models.js
+++ js/models.js
@@ -1,18 +1,37 @@
 const models = {
     "DreamShaper": {
         "description": "A good generalist model",
-        "tests": ["animal", "landscapes", "people"]
+        "tests": ["animals", "landscapes", "people"],
+        "official_link": "https://civitai.com/models/4384/dreamshaper"
     },
     "FreedomRedmond": {
         "description": "A good generalist model",
-        "tests": ["animal", "landscapes", "people"]
-    },
-    "AnalogMadness": {
-        "description": "A realistic model, specialized in real photographies. Be careful, it can generate NSFW content even if you tell it not to do in negative prompts.",
-        "tests": ["animal", "landscapes", "people"]
+        "tests": ["animals", "landscapes", "people"],
+        "official_link": "https://civitai.com/models/87288"
     },
     "GhostMix": {
         "description": "A model specialized in anime pictures. Likes to generates girls.",
-        "tests": ["animal", "landscapes", "people"]
+        "tests": ["animals", "landscapes", "people"],
+        "official_link": "https://civitai.com/models/36520/ghostmix"
     }
 }
+const styles_exp = [
+    "((anime style))",
+    "modern art, artistic",
+    "3d render, 3d",
+    "unreal engine render, 3d render",
+    "realistic, real, photography",
+    "((perfect composition))",
+    "((masterpiece))",
+    "((intense))",
+    "((colorful))",
+    "((wonderful))",
+    "((vibrant colors))",
+    "flat colors, 2d colors",
+    "fluffy, fur",
+    "bright, bright lighting",
+    "epic, epic lighting",
+    "((dark))",
+    "mysterious, mystery",
+    "intricate, detailed, ultra detailed"
+];
